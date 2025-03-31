7f3312870cf23f516e844b9736e6c0b7c01ca0b9
Siv <shivasudharsank2008@gmail.com>
2025-03-31 14:47:00 +0530

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+ANILIST_API_URL = "https://graphql.anilist.co"
+
+def get_anime_data():
+    query = """
+    query {
+      Page(page: 1, perPage: 10) {
+        media(season: WINTER, seasonYear: 2025, type: ANIME) {
+          title {
+            romaji
+            english
+          }
+          description
+          coverImage {
+            large
+          }
+          siteUrl
+        }
+      }
+    }
+    """
+    response = requests.post(ANILIST_API_URL, json={"query": query})
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["Page"]["media"]
+    else:
+        return []
+
+@app.route('/')
+def home():
+    anime_list = get_anime_data()
+    return render_template('index.html', anime_list=anime_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
